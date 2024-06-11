e1f41e2848e81cc9faaaeba2d976e3d6690a0e72
gornostaev <gornostaev.s19@gmail.com>
2024-06-11 11:52:56 +0300

    баг

--- company.birzha-leads.com/app/Controllers/ZvonokController.php
+++ company.birzha-leads.com/app/Controllers/ZvonokController.php
@@ -35,7 +35,7 @@ class ZvonokController extends Controller
         $request = ValidationUtil::validate($_GET,[
             "phone" => 'max:255',
             "projectId" => 'integer|default:102862',
-            "tag" => 'max:255' // {ct_campaign_id}
+            "tag" => 'max:255|default:\'\'' // {ct_campaign_id}
         ]);
 
         $this->zvonokClientRepository->save(ZvonokClient::make(
