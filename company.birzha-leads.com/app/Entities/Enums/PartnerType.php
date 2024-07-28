bd71b014b04345ded8fec025411ce592190453ed
gornostaev <gornostaev.s19@gmail.com>
2024-07-28 17:51:59 +0300

    партнерка

--- company.birzha-leads.com/app/Entities/Enums/PartnerType.php
+++ company.birzha-leads.com/app/Entities/Enums/PartnerType.php
@@ -7,13 +7,17 @@ enum PartnerType: int
     case default = 0;
     case local = 1;
     case federal = 2;
+    case Fris = 3;
+    case Korochentsev = 4;
 
     public static function getLabel(int $enum): string
     {
         return match ($enum) {
             self::default->value => '-',
             self::local->value => 'Локальная',
-            self::federal->value => 'Федеральная'
+            self::federal->value => 'Федеральная',
+            self::Fris->value => 'Фрис',
+            self::Korochentsev->value => 'Короченцев'
         };
     }
 }
